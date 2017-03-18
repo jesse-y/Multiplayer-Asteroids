d06427fb715d1cb51f92fad732998a537f7caa8b
jesse-y <jesse.yue@gmail.com>
2017-03-19 02:52:44 +1100

    canvas and layout business

--- asteroids.js
+++ asteroids.js
@@ -1,16 +1,22 @@
 var ws;
 $().ready(function () {
 	//change status code check if javascript has successfully loaded
-	$('#status').text('sciprts loaded')
+	$('#status').text('scripts loaded');
 
-	$('#btnConnect').on('click', function() {
+	var app = new Asteroids_App(document.getElementById('app'), 640, 480);
+
+
+	$('#btnConnect, #btnConnectLocal').on('click', function() {
 		$("#status").text("connecting");
 
-		var ws_url = "ws://hawkleon.com:8008/connect";
+		if (this.id == 'btnConnectLocal') {
+			var ws_url = "ws://localhost:8080/connect";
+		} else {
+			var ws_url = "ws://hawkleon.com:8008/connect";
+		}
 		ws = new WebSocket(ws_url);
 		ws.onopen = function() {
 			$('#status').text("connected");
-
 		};
 		ws.onmessage = function (e) {
 			$('#status').text(e.data);
@@ -39,4 +45,20 @@ $().ready(function () {
 	$('#btnDisconnect').on('click', function() {
 		ws.close();
 	})
-})
+});
+
+function Asteroids_App(elem, width, height) {
+	this.canvas = document.createElement('canvas');
+	this.ctx = this.canvas.getContext('2d');
+	this.canvas.id = 'viewport';
+	//this.canvas.height = height;
+	//this.canvas.width = width;
+	elem.appendChild(this.canvas);
+
+	this.start_page();
+};
+
+Asteroids_App.prototype.start_page = function() {
+	this.ctx.fillStyle = '#D9D9D9';
+	this.ctx.fillRect(0,0,this.canvas.width, this.canvas.height);
+}
