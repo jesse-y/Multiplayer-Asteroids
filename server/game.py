684b5fd86307101e968f9583a1fd823fa5f6260d
jesse-y <jesse.yue@gmail.com>
2017-03-23 11:27:00 +1100

    moved user manager logic into server file. fixed bugs with adding and removing players, and adding and removing games

--- server/game.py
+++ server/game.py
@@ -10,17 +10,33 @@ class Game:
 
 		self.tick = 0
 
+	def __hash__(self):
+		return hash(self.game_id)
+
 	def new_player(self, user):
-		self.players[user.uid] = user
+		self.players[user] = user
 		if len(self.players) == settings.MAX_PLAYERS:
-			for player in self.players.values():
-				print('game>new_player: added new player')
-				msg = 'player "{}" has joined the game'.format(player.username)
-				player.ws.send_str(msg)
 			self.ready = True
+		for player in self.players.values():
+			print('game_{}>p{}={}: added new player'.format(self.game_id, player.uid, player.username))
+			msg = 'player "{}" has joined the game'.format(player.username)
+			player.ws.send_str(msg)
+
+	def disconnect_player(self, player):
+		if player in self.players:
+			del self.players[player]
+		if len(self.players) < 1:
+			self.finished = True
+			return
+		for ingame_player in self.players.values():
+			print('game_{}>disconnect_player: send_all disconnect player'.format(self.game_id))
+			msg = '{} has disconnected'.format(player.username)
+			ingame_player.ws.send_str(msg)
 
 	def next_frame(self):
-		for user in self.players.values():
-			print('game>next_frame: sending game tick')
+		if self.finished:
+			return
+		for player in self.players.values():
+			print('game_{}>next_frame: sending game tick to player_{}'.format(self.game_id, player.uid))
 			self.tick += 1
-			user.ws.send_str('game_{}: tick {}'.format(self.game_id, self.tick))
+			player.ws.send_str('game_{}: tick {}'.format(self.game_id, self.tick))
--- server/server.py
+++ server/server.py
@@ -1,9 +1,12 @@
 import asyncio
+from asyncio import Queue
 from aiohttp import web
 import json
 
 import settings
-from user_manager import UserManager
+from id_manager import IdManager, IdManagerException
+from user import User
+from game import Game
 
 async def handle(request):
 	print ('got request: {}'.format(request))
@@ -14,7 +17,6 @@ async def handle(request):
 
 async def wshandler(request):
 	app = request.app
-	um = app['user_manager']
 	ws = web.WebSocketResponse()
 	await ws.prepare(request)
 
@@ -29,21 +31,68 @@ async def wshandler(request):
 				continue
 			if not user:
 				if data[0] == 'new_user':
-					user = um.new_user(data[1], ws)
+					user = new_user(app, data[1], ws)
 			else:
 				ws.send_str('Server: message={}'.format(msg.data))
 		elif msg.tp == web.MsgType.close or msg.tp == web.MsgType.error:
 			break
 
-	um.remove_user(user)
-	print('disconnect: uid={}, username={}'.format(user.uid, user.username))
+	if user:
+		disconnect_user(app, user)
+		print('disconnect: uid={}, username={}'.format(user.uid, user.username))
 	return ws
+
+async def manage_users(app):
+	game = None
+	while True:
+		user = await app['searching'].get()
+
+		if not game:
+			gid = app['gidm'].assign_id()
+			game = Game(game_id=gid)
+			app['games'][game] = game
+		
+		game.new_player(user)
+		app['in_game'][user] = game
+
+		if game.ready:
+			asyncio.ensure_future(game_loop(app, game))
+			game = None
+		await asyncio.sleep(1/30)
+
+async def game_loop(app, game):
+	while True:
+		game.next_frame()
+		if game.finished:
+			break
+		await asyncio.sleep(2)
+	print('game_{}> finished'.format(game.game_id))
+	del app['games'][game]
+	app['gidm'].release_id(game.game_id)
+	for player in game.players.values():
+		player.ws.close()
+
+def new_user(app, username, ws):
+	uid = app['uidm'].assign_id()
+	user = User(uid=uid, username=username, ws=ws)
+	app['searching'].put_nowait(user)
+	return user
+
+def disconnect_user(app, user):
+	app['uidm'].release_id(user.uid)
+	if user in app['in_game']:
+		app['in_game'][user].disconnect_player(user)
 
 if __name__ == '__main__':
 	app = web.Application()
-	app['user_manager'] = UserManager()
+	
+	app['in_game'] = {}
+	app['games'] = {}
+	app['searching'] = Queue(maxsize=settings.MAX_USERS)
+	app['uidm'] = IdManager(max_id=settings.MAX_USERS)
+	app['gidm'] = IdManager()
 
-	asyncio.ensure_future(app['user_manager'].manage())
+	asyncio.ensure_future(manage_users(app))
 
 	app.router.add_route('GET', '/connect', wshandler)
 	app.router.add_route('GET', '/', handle)
