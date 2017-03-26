18f7e5fc432c5abd730b7fbf988c435e61aff83c
jesse-y <jesse.yue@gmail.com>
2017-03-27 03:34:12 +1100

    choice branch

--- scripts/support.js
+++ scripts/support.js
@@ -1,6 +1,14 @@
 (function() {
 	window.print_msg = function(id, message) {
-		document.getElementById(id).innerHTML = message;
+		var element = document.getElementById(id);
+		if (element == undefined) {
+			element = document.createElement('div');
+			element.id = id;
+			element.innerHTML = message;
+			document.body.appendChild(element);
+		} else {
+			document.getElementById(id).innerHTML = message;
+		}
 	};
 	window.show = function(id) {
 		document.getElementById(id).style.display = 'block';
