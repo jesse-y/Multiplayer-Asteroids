46bc34b3f7fe846544931e4e0add6b6d7266d9bb
jesse-y <jesse.yue@gmail.com>
2017-06-27 18:06:51 +1000

    fixed bugs with setup.py

--- setup.py
+++ setup.py
@@ -5,34 +5,34 @@ import http.client as httplib, urllib, sys
 print('Creating index.html file ...')
 
 index_file = [
-	'<!DOCTYPE html>',
-	'<html>',
-		'<head>',
-			'<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />',
-			'<title>Asteroids Multiplayer</title>',
-			'<link rel="stylesheet" type="text/css" href="style.css" />',
-		'</head>',
-		'<body>',
-			'<div id="page">',
-				'<div id="status">Status</div>',
-				'<div id="network">network info</div>',
-				'<div id="app"></div>',
-				'<p> Move using WASD keys </p>',
-				'<p> Shoot with LMB </p>',
-				'<p> Your ship can take 3 points of damage before you are vulnerable to enemy fire! Your shields will recharge after 3 seconds as long as you don't take any additional damage. </p>',
-				'<p> Fire a Rocket with V. The rocket will target and home in on the player closest to your cursor. You can store up to 2 rockets at a time. Rockets take 3 seconds to recharge. Rockets pierce shields. </p>',
-				'<p> Destroying large and medium asteroids will occassionally drop invulnerability powerups. Press SPACE to use. Lasts 2 seconds. </p>',
-				'<p> Good luck out there! </p>',
-				'<p> (C) Jesse Yue 2017',
-			'</div>',
-		'</body>',
-		'<script type="text/javascript" src="js/app.js"></script>',
-	'</html>'
+'<!DOCTYPE html>',
+'<html>',
+'	<head>',
+'		<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />',
+'		<title>Asteroids Multiplayer</title>',
+'		<link rel="stylesheet" type="text/css" href="style.css" />',
+'	</head>',
+'	<body>',
+'		<div id="page">',
+'			<div id="status">Status</div>',
+'			<div id="network">network info</div>',
+'			<div id="app"></div>',
+'			<p> Move using WASD keys </p>',
+'			<p> Shoot with LMB </p>',
+'			<p> Your ship can take 3 points of damage before you are vulnerable to enemy fire! Your shields will recharge after 3 seconds as long as you don\'t take any additional damage. </p>',
+'			<p> Fire a Rocket with V. The rocket will target and home in on the player closest to your cursor. You can store up to 2 rockets at a time. Rockets take 3 seconds to recharge. Rockets pierce shields. </p>',
+'			<p> Destroying large and medium asteroids will occassionally drop invulnerability powerups. Press SPACE to use. Lasts 2 seconds. </p>',
+'			<p> Good luck out there! </p>',
+'			<p> (C) Jesse Yue 2017',
+'	</div>',
+'	</body>',
+'	<script type="text/javascript" src="js/app.js"></script>',
+'</html>'
 ]
 
 with open('index.html', 'w') as output:
 	for line in index_file:
-		output.write(line)
+		output.write(line+'\n')
 
 #-----COLLECT AND CONCATENATE JAVASCRIPT FILES----------------------//
 
@@ -63,7 +63,7 @@ params = urllib.parse.urlencode([
     ('output_info', 'compiled_code'),
   ])
 
-print('Project length: {} char'.format(len(js_code)))
+print('Project length: {} bytes'.format(len(js_code)))
 print('Compiling code ...')
 # Always use the following value for the Content-type header.
 headers = { "Content-type": "application/x-www-form-urlencoded" }
@@ -71,7 +71,7 @@ conn = httplib.HTTPConnection('closure-compiler.appspot.com')
 conn.request('POST', '/compile', params, headers)
 response = conn.getresponse()
 data = response.read()
-print('Compiled length: {} char'.format(len(data)))
+print('Compiled length: {} bytes'.format(len(data)))
 with open('app.js', 'wb') as output:
 	output.write(data)
 conn.close()
